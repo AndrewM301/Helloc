d2e3088f716aabfe649ed5c35dccf251b174539e
AndrewM301 <amarshall30@huskers.unl.edu>
2026-08-27 18:08:32 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author Andrew Marshall
+ *Basic hello world program
+ *
+ */
+
+ int main(int argc, char **argc){
+
+    printf("Hello, Im Andrew Marshall/n");
+    printf("Im a Computer Science Major/n");
+
+    return 0;
+ }
+
